cc1949a42a2e84d140734d1897592bb9fa69c500
Deepak Prabhakara <deepak@boxyhq.com>
2023-10-21 22:01:02 +0100

    updated jackson-demo to sso.eu.boxyhq.com

--- lib/env.ts
+++ lib/env.ts
@@ -8,7 +8,7 @@ export const env = {
   // BoxyHQ SAML Jackson
   jackson: {
     url:
-      process.env.BOXYHQ_SAML_JACKSON_URL || "https://jackson-demo.boxyhq.com",
+      process.env.BOXYHQ_SAML_JACKSON_URL || "https://sso.eu.boxyhq.com",
     apiKey: process.env.BOXYHQ_SAML_JACKSON_API_KEY || "secret",
     product: process.env.BOXYHQ_PRODUCT || "jackson-cerbos",
     redirectUrl: `${process.env.APP_URL}/login`,
